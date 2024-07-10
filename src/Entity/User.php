09269f6099fb4251cb6f2e9edb575bf3cf07103e
Agustin Quintero <agustin.quintero@limayrac.fr>
2024-07-10 15:58:56 +0200

    feat: table forgetpassword

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -39,6 +41,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255)]
     private ?string $prenom = null;
+
+    /**
+     * @var Collection<int, ForgetPassword>
+     */
+    #[ORM\OneToMany(targetEntity: ForgetPassword::class, mappedBy: 'user', orphanRemoval: true)]
+    private Collection $forgetPasswords;
+
+    public function __construct()
+    {
+        $this->forgetPasswords = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -135,6 +148,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPrenom(string $prenom): static
     {
         $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ForgetPassword>
+     */
+    public function getForgetPasswords(): Collection
+    {
+        return $this->forgetPasswords;
+    }
+
+    public function addForgetPassword(ForgetPassword $forgetPassword): static
+    {
+        if (!$this->forgetPasswords->contains($forgetPassword)) {
+            $this->forgetPasswords->add($forgetPassword);
+            $forgetPassword->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeForgetPassword(ForgetPassword $forgetPassword): static
+    {
+        if ($this->forgetPasswords->removeElement($forgetPassword)) {
+            // set the owning side to null (unless already changed)
+            if ($forgetPassword->getUser() === $this) {
+                $forgetPassword->setUser(null);
+            }
+        }
 
         return $this;
     }
