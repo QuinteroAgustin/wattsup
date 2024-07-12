6b5178dc2532619a3beb3ba365c2d588fc8cb5fe
Agustin Quintero <agustin.quintero@limayrac.fr>
2024-07-12 09:26:06 +0200

    fix:remove vue

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,6 @@ Encore
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
-    .enableVueLoader()
-
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
@@ -74,7 +72,6 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
     .enablePostCssLoader()
-    .enableVueLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
