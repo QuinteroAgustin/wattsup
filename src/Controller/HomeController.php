e3f2c41d83627cbd04292a735ba45bd98026abc4
Agustin Quintero <agustin.quintero@limayrac.fr>
2024-07-12 16:30:18 +0200

    fix:securityMiddlewareHome

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+    
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
+        // Si l'utilisateur est déjà authentifié, le rediriger vers la page de chat
+        if ($this->security->getUser()) {
+            return new RedirectResponse($this->generateUrl('app_chat'));
+        }
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
